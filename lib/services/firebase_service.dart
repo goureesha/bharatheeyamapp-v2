1ebdaefb4fbc3e8424a3884df63fd447fbce6a68
Bharatheeyam <bharatheeyam@app.com>
2026-03-27 22:20:58 +0530

    fix: explicit foundation import for kIsWeb resolution to fix CI build

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import '../main.dart';
-import 'google_auth_service.dart';
 import 'google_auth_service.dart';
 import 'appointment_service.dart';
 
