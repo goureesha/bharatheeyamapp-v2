83b3f683a17168963e3a647d1c687299e074d628
goureesha <goureesha@users.noreply.github.com>
2026-04-19 13:56:35 +0000

    deploy: 638a3c002810393425b934489df5f935057d4a6e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,8 +37,8 @@ const RESOURCES = {"assets/assets/images/logo.png": "8b3be256f8c343bfe505d1de18b
 "canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
 "privacy_policy.html": "10094331e2887570d5e079618ca2fdc8",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1f277be46686eae8214f109b69681ac4",
-"main.dart.js": "6782a9e26402052702f1aab74fbb0e64",
+"flutter_bootstrap.js": "f2db67934ed4e642c13194bd8034f5ab",
+"main.dart.js": "8b882ce46e35a7268ad9ec9c20cfcef9",
 "version.json": "40d383273be60e5774161ecd3d63e663",
 "googled477f94eebb558c3.html": "b609deddf10e215d559e8f07f04425df"};
 // The application shell files that are downloaded before a service worker can
